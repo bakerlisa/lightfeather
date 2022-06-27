bb2992cde748cdc2c325283553cf430e09bcb4ef
Lisa <lb.broadhead@gmail.com>
2022-06-27 15:55:04 -0500

    first commit

--- client/src/components/views/Popup.js
+++ client/src/components/views/Popup.js
@@ -1,10 +1,77 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useSyncExternalStore } from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from '../css/Popup.module.scss'
 import axios from 'axios';
 
 const Popup = () => {
-    axios.get('https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers').then(response=>{
-        console.log(response);
+    const history = useHistory();
+
+    const [supervisor, setSupervisor] = useState([]);
+    const [form, setForm] = useSyncExternalStore({})
+
+    const lengths = {
+        firstName: 3,
+        lastName: 3,
+        phoneNumber: 10
+    }
+
+    const [error,setError] = useState({
+        firstName: false,
+        lastName: false,
+        phoneNumber: false
+    })
+
+    const [dbError,setDBError] = useState({ id:0 })
+    const [errMessage, setErrMessage] = useState(" ")
+    var errorSize = Object.keys(dbError).length;
+
+    function ValidateEmail(event) {
+        setForm({...form,[event.target.name]:event.target.value})
+
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(event.target.value)){
+            setError({...error, email: true})
+        }else{
+            setError({...error, email: false})
+        }
+    }
+
+    const onChangeHandler = (event) => {
+        setForm({...form,[event.target.name]: event.target.value})
+
+        if(event.target.name in error){
+            if(event.target.value.length >= lengths[event.target.name]){
+                setError({...error,[event.target.name]:true})
+            }else{
+                setError({...error,[event.target.name]:false})
+            }
+        }
+    }
+
+    // sorts supervisors
+    const handleSort = (data) =>{
+        setSupervisor(data
+            .sort((a,b) => {
+                if(a.jurisdiction === b.jurisdiction){
+                    if(a.lastName === b.lastName){
+                        // if first names match sort by first name
+                        return a.firstName > b.firstName ? 1: -1
+                    }else{
+                        // last names match sort by last names
+                        return a.lastName > b.lastName ? 1: -1
+                    }
+                }
+                // if jurisdiction's match sort by jurisdiction
+                return a.jurisdiction > b.jurisdiction ? 1: -1
+            })
+        )
+    }
+
+    useEffect(() => {
+        axios.get('https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers')
+        .then(response=>{
+            setSupervisor(response.data)
+            handleSort(response.data)
+        })
     })
     return (
         <div className={styled.popWrp}>
@@ -13,41 +80,46 @@ const Popup = () => {
                 <p className={styled.required}>* Required Fields</p>
 
                 <div>
-                    <label htmlFor="fname">*First Name</label>
-                    <input type="text" name="fname" id="" placeholder="First Name" />
+                    <label htmlFor="firstName">*First Name</label>
+                    <input type="text" name="firstName" placeholder="First Name" />
                 </div>
 
                 <div>
-                    <label htmlFor="lname">*Last Name</label>
-                    <input type="text" name="lname" id="" placeholder="Last Name" />
+                    <label htmlFor="lastName">*Last Name</label>
+                    <input type="text" name="lastName" placeholder="Last Name" />
                 </div>
 
                 <h4>How woud you prefer to be notified?</h4>
                 <div className="flex">
                     <div className={styled.col_2}>
                         <div>
-                            <input type="checkbox" name="econtact" id="" />
+                            <input type="checkbox" name="econtact" />
                             <label htmlFor="email">*Email</label>
                         </div>
-                        <input type="email" name="email" id="" placeholder="Email: example@email.com..."/>
+                        <input type="email" name="email" placeholder="Email: example@email.com..."  onChange={ValidateEmail} />
                     </div>
 
                     <div className={styled.col_2}>
                         <div>
-                            <input type="checkbox" name="tcontact" id="" />
-                            <label htmlFor="number">Phone:</label>
+                            <input type="checkbox" name="tcontact" />
+                            <label htmlFor="phoneNumber">Phone:</label>
                         </div>
-                        <input type="tel" name="number" id="" placeholder="Phone Number..." />
+                        <input type="tel" name="phoneNumber" placeholder="Phone Number..." />
                     </div>
                 </div>
 
                 <div>
                     <label htmlFor="supervisor">*Supervisor</label>
-                    <select name="supervisor" id="" defaultValue="default">
+                    <select name="supervisor" defaultValue="default">
                         <option value="default" disabled >Select...</option>
+                        {supervisor.length === 0 ? "" : supervisor.map((item,i) => {
+                            if(parseInt(item.jurisdiction) || parseInt(item.jurisdiction) === 0) {
+                            }else{
+                                return <option key={i} value={item.id}>{item.jurisdiction} - {item.lastName}, {item.firstName}</option>
+                            }
+                        })}
                     </select>
                 </div>
-
                 <input type="submit" value="Submit" className={styled.submit} />
             </form>
         </div>
